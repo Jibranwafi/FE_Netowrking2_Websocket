ecc1b3dd153d18ccdb424bf69fd822e0cf43f182
Jibranwafi <71427316+Jibranwafi@users.noreply.github.com>
2022-01-13 23:06:33 +0700

    first commit

---
+++ src/main/java/com/fe/networking/websocketnetworking2/WebsocketNetworking2Application.java
@@ -0,0 +1,13 @@
+package com.fe.networking.websocketnetworking2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketNetworking2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketNetworking2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fe/networking/websocketnetworking2/WebsocketNetworking2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fe.networking.websocketnetworking2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketNetworking2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
